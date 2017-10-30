a59f1d764c6c58d85ddd548113910c9e0114d81a
nosmokingsurfer <nosmokingsurfer@gmail.com>
2017-10-30 14:14:28 +0300

    mercator fix

--- scripts/mercator.py
+++ scripts/mercator.py
@@ -2,13 +2,14 @@ import math
 import numpy as np
 
 def Mercator(data):
-    #data массив точек. Каждая строка это пара [E,N] и менно в таком виде
-    start_latitude = data[1]
-    phi = start_latitude*np.pi/180 #start_latitude - широта с которой мы начинаем трек
+    #data[0] = [east, north]
+    start_latitude = data[0][1] #the latitude we start from
+    print(start_latitude)
+    phi = start_latitude*np.pi/180.0
     R_equator = 6378137.0
-    s = cos(phi)
-    R = R_equator * 1 * (0.99832407 + 0.00167644 * cos(2 * phi) - 0.00000352*cos(4 * phi))
-	
-    x_meters = s * R * np.pi * data[:,1] / 180
-    y_meters = s * R * np.log(np.tan(np.pi * (90 + data[:,0]) / 360))
+    s = np.cos(phi)
+    R = R_equator * 1 * (0.99832407 + 0.00167644 * np.cos(2 * phi) - 0.00000352*np.cos(4 * phi))
+    
+    x_meters = s * R * np.pi * data[:,0] / 180.0
+    y_meters = s * R * np.log(np.tan(np.pi * (90.0 + data[:,1]) / 360.0))
     return [x_meters, y_meters]
